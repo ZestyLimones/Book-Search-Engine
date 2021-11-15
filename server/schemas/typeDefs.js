34542debddb838b51bdfc0f8a1cb588c9651acf1
Micheal Limones <micheal@zestylimones.com>
2021-11-15 09:39:47 -0700

    added content to typeDefs file.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,38 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    books: [Book]
+  }
+
+  type Book {
+    authors: [Author]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Author {
+    name: String
+  }
+
+  type Query {
+    users: [User]
+    user(username: String): User
+    books: [Book]
+    book(title: String): Book
+    authors: [Author]
+  }
+
+  #   type Mutation {
+  #       addUser(username)
+  #   }
+`;
 
 module.exports = typeDefs;
