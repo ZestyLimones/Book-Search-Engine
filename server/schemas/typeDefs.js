176a7c25872412623f57c1ab214ff4c08d621e9e
Micheal Limones <micheal@zestylimones.com>
2021-11-16 13:04:22 -0700

    updated content in typeDefs. added mutations.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,33 +6,45 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
-    books: [Book]
+    bookCount: Number
+    savedBooks: [Bood]
   }
 
   type Book {
+    _id: ID
+    bookId: Number
     authors: [Author]
     description: String
-    bookId: String
     image: String
     link: String
     title: String
   }
 
   type Author {
+    _id: ID
     name: String
   }
 
-  type Query {
-    users: [User]
-    user(username: String): User
-    books: [Book]
-    book(title: String): Book
-    authors: [Author]
+  type Auth {
+    token: ID!
+    user: User
   }
 
-  # type Mutation {
-  #     addUser(username: String!, email: String!, password: String!): Auth
-  # }
+  type Query {
+    # users: [User]
+    # user(username: String): User
+    # books: [Book]
+    # book(title: String): Book
+    # authors: [Author]
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    savedBook(bookId: Number!): Book
+    removeBook(bookId: Number!): Book
+  }
 `;
 
 module.exports = typeDefs;
