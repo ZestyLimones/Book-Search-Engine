6c7c02b6fdc5bda3632e94972fcdde5253437da1
Micheal Limones <micheal@zestylimones.com>
2021-11-17 09:57:09 -0700

    added missing backtick.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,14 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-query me {
-  me {
-    _id
-    username
-    email
-    password
-    bookCount
-    savedBooks
+  query me {
+    me {
+      _id
+      username
+      email
+      password
+      bookCount
+      savedBooks
+    }
   }
-}
+`;
