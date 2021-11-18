c920796cf2852103e880a03fd4ad67fc3d3eccfb
Micheal Limones <micheal@zestylimones.com>
2021-11-17 17:50:59 -0700

    removed commments.

--- server/server.js
+++ server/server.js
@@ -20,7 +20,6 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -35,16 +34,10 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
   useCreateIndex: true,
   useFindAndModify: false,
 });
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
